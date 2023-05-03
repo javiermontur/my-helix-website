72c5162d7dda79d33f22a65b325354c8c833afd7
jose javier montero <javiermontur@gmail.com>
2023-05-03 15:23:19 -0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -55,8 +55,8 @@ export function decorateMain(main) {
   decorateIcons(main);
   buildAutoBlocks(main);
   decorateSections(main);
-  normalizeHeadings(main);
   decorateBlocks(main);
+  normalizeHeadings(main, 'h1, h2, h3, h4, h5, h6');
 }
 
 /**
